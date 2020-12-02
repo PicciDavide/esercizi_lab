3ef94ed648a915493d625f4f20fb6e65cf93c1d1
Davide Piccinini <72320775+PicciDavide@users.noreply.github.com>
2020-12-02 19:47:44 +0100

    Update esercizio_30.py

--- esercizio_30.py
+++ esercizio_30.py
@@ -7,10 +7,9 @@ for i in range(length):
     if numero == 0 or 1:
         n_binario += str(numero)
         if numero == 1:
-            n_decimale += 2**(length - i)
+            n_decimale += 2**i
     else:
         print("I numeri binari sono fatti solo da 0 e 1")
         i -= 1
-for i in range(8 - length):
-    n_binario += "0"
-print("Convertito in decimale il numero è:", n_decimale, "(la funzione di python lo converte come:", int(bin(n_decimale), 2)), ")"
+n_binario = n_binario[::-1]
+print("Convertito in decimale il numero è:", n_decimale, "(la funzione di python lo converte come:", int(n_binario, 2), ")")
