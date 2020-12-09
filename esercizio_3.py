d12c25c4a4cd54b75d8e635317a94aec9031bec9
Davide Piccinini <72320775+PicciDavide@users.noreply.github.com>
2020-12-09 17:52:27 +0100

    Update esercizio_3.py

--- esercizio_3.py
+++ esercizio_3.py
@@ -1,17 +1,22 @@
-parola_r = input("Scrivere la parola in rövarspräket: ")
-vocali = "aeiou"
-counter = -1
-cons_counter = 0
-cons_toggle = 3
-parola = ""
-for l in parola_r:
-    counter += 1
-    if l not in vocali:
-        cons_counter += 1  
-    if cons_toggle == 0:
-        cons_counter = 0
-    if cons_counter == 2:
-        if parola_r[l] == parola_r[l - 2]:
-            parola = parola + parola_r[l]
-    cons_toggle -= 1
-print(parola)
+restart = True
+while restart:
+    controllo_domanda = True
+    parola_i = input("Scrivere la parola da tradurre in rövarspräket: ").lower()
+    vocali = "bcdfghjklmnpqrstwxyz"
+    parola = ""
+    for l in parola_i:
+        if l in vocali:
+            parola = parola + l + "o" + l
+        else:
+            parola = parola + l
+    print("traduzione:", parola)
+    while controllo_domanda == True:
+        print("Vuoi tradurre un altra parola?")
+        risposta = input("Si      No\n").lower()
+        if risposta == "si":
+            controllo_domanda = False
+        elif risposta == "no":
+            restart = False
+            controllo_domanda = False
+        else:
+            print("Puoi rispondere con Si o No")
