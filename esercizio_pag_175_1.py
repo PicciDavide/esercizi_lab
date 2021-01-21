ae2c35da1663a6e2c3e108ef31d2f4bbf9bf09e4
Davide Piccinini <72320775+PicciDavide@users.noreply.github.com>
2021-01-21 10:40:11 +0100

    coso da finire

---
+++ esercizio_pag_175_1.py
@@ -0,0 +1,13 @@
+giorni_ita = {"lunedì":1, "martedì":2, "mercoledì":3, "giovedì":4, "venerdì":5, "sabato":6, "domenica":7}
+elenco = sorted(giorni_ita.values())
+
+elenco_ita = giorni_ita.items()
+print(elenco_ita,"s")
+
+for i in elenco:
+    for item in elenco_ita:
+        if item[1] = 
+    print("Traduci", giorno, "in inglese: ")
+    giorno_ingl = input()
+    giorni_ita[giorno] = giorno_ingl
+print(giorni_ita.items())
